6143e342a3c05d7b68f74fa75163fe8c545836e8
Surasit <surasit44445@gmail.com>
2022-08-17 09:43:34 +0700

    Update

---
+++ src/main/java/com/sheet/google/service/GoogleSheetsService.java
@@ -0,0 +1,8 @@
+/*package com.sheet.google.service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public interface GoogleSheetsService {
+    void getSpreadsheetValues() throws IOException, GeneralSecurityException;
+}*/
